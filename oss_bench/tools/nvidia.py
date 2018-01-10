58b107f4f1ee500179fbfe0773d1382ccd021e95
Toby Boyd <tobyboyd@google.com>
2018-01-09 16:56:15 -0800

    Check for running processes via nvidia-smi.

--- oss_bench/tools/nvidia.py
+++ oss_bench/tools/nvidia.py
@@ -1,5 +1,7 @@
 """Extract information about the system GPU."""
 from __future__ import print_function
+import local_command
+import re
 import subprocess
 
 
@@ -14,7 +16,7 @@ def get_gpu_info():
     Tuple of device driver version and gpu name.
   """
   cmd = 'nvidia-smi --query-gpu=driver_version,gpu_name --format=csv'
-  retcode, result = _run_local_command(cmd)
+  retcode, result = local_command.run_local_command(cmd)
   lines = result.splitlines()
   if retcode == 0 and len(lines) > 1:
     gpu_info = lines[1].split(',')
@@ -28,19 +30,48 @@ def get_gpu_info():
     return '', ''
 
 
-def _run_local_command(cmd):
-  """Structures for a variety of different test results.
+def get_running_processes():
+  """Returns list of `dict` objects representing running processes on GPUs."""
+  retcode, result = local_command.run_local_command('nvidia-smi')
+  lines = result.splitlines()
+  if retcode == 0 and len(lines) > 1:
+    # Goes to the first line with the word Processes, jumps down one and then
+    # parses the list of processes.
+    look_for_processes = False
+    processes = []
+    for line in lines:
+      # Summary line starts with images/sec
+      if line.find('Processes') > 0:
+        look_for_processes = True
 
-  Args:
-    cmd: Command to execute
-  Returns:
-    Tuple of the command return value and the standard out in as a string.
+      if look_for_processes:
+        p = re.compile('[0-1]+')
+        m = p.search(line)
+        if m and m.span()[0] == 5:
+          line_parts = line.strip().replace('|', '').split()
+          processes.append(line_parts)
+
+    return processes
+
+  else:
+    print('nvidia-smi did not return as expected:{}'.format(result))
+    return '', ''
+
+
+def is_ok_to_run():
+  """Returns true if the system is free to run GPU tests.
+
+  Checks the list of processes and if the list is empty or if the list of
+  processes does not contain actual ML jobs returns true.  Non-ML Jobs
+  like 'Xorg' or even 'cinnamon' are not a problem.
+
+  Note: Currently this method returns true if no python processes are found.
+    Which seems more sane than a long list of processes that do not matter. On
+    clean systems the process list should be and normally is zero.
   """
-  p = subprocess.Popen(
-      cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-  stdout = ''
-  while True:
-    retcode = p.poll()
-    stdout += p.stdout.readline()
-    if retcode is not None:
-      return retcode, stdout
+  processes = get_running_processes()
+  for process in processes:
+    # Checks process name position for process named 'python'.
+    if 'python' == process[3]:
+      return False
+  return True
