4e40cd9b4a0597da732257050d5f2922e7ba8a18
Toby Boyd <tobyboyd@google.com>
2018-08-10 12:29:19 -0700

    num_epochs and fix num_gpus to gpus

--- oss_bench/test_runners/tf_cnn_bench/command_builder.py
+++ oss_bench/test_runners/tf_cnn_bench/command_builder.py
@@ -27,7 +27,7 @@ def build_run_command(run_config):
       'datasets_prefetch_buffer_size', 'datasets_prefetch_threadpool_size',
       'datasets_num_private_threads', 'variable_consistency',
       'compact_gradient_transfer', 'network_topology', 'train_dir',
-      'summary_verbosity', 'save_summaries_steps'
+      'summary_verbosity', 'save_summaries_steps', 'num_epochs'
   ]
 
   for arg in pass_through_args:
