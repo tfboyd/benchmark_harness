a9f36d30765b63e72db4a47ec7f09d7957089cae
Toby Boyd <tobyboyd@google.com>
2018-09-29 16:56:41 -0700

    Add gpu_count and group_run_id

--- oss_bench/tools/nvidia_test.py
+++ oss_bench/tools/nvidia_test.py
@@ -26,6 +26,15 @@ class TestNvidiaTools(unittest.TestCase):
     self.assertEqual('GTX 1080', gpu_info)
 
   @patch('tools.local_command.run_local_command')
+  def test_get_gpu_count(self, run_local_command_mock):
+    """Tests gpu info returns second entry if first entry is a Quadro."""
+    run_local_command_mock.return_value = [
+        0, 'blah\n200.99, Quadro K900 \n381.99, GTX 1080\n'
+    ]
+    gpu_count = nvidia.get_gpu_count()
+    self.assertEqual(2, gpu_count)
+
+  @patch('tools.local_command.run_local_command')
   def test_is_ok_to_run_false(self, run_local_command_mock):
     """Tests ok_to_run finding existing processes."""
     smi_test = 'tools/test_files/example_nvidia-smi_processes.txt'
